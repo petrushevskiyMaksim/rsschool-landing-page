62fbea3355c5d198b1c6df0ee843d8d8c19ea245
Petrushevskiy Maxim <maxinverst@gmail.com>
2026-09-21 19:26:08 +0500

    feat: add landing page

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import path from 'path';
+
+export default defineConfig({
+    base: '/rsschool-landing-page/',
+    css: {
+        devSourcemap: true,
+        preprocessorOptions: {
+            scss: {
+                sourceMap: true, // Обязательно для корректной работы карт в SCSS
+                cssMinify: false,
+            },
+        },
+    },
+
+    plugins: [
+        createSvgIconsPlugin({
+            // Папка, в которой лежат иконки
+            iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+            // Формат ID, который будет использоваться в <use>
+            symbolId: 'icon-[name]',
+        }),
+    ],
+});
